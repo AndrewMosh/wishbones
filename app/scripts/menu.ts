4d27dd5b4068c0a44ac7f59f61b36120f33c8c46
Andrew Mosh <malinaenglishclub@gmail.com>
2024-07-09 17:29:51 +0300

    mobile menu fixed

--- app/scripts/menu.ts
+++ app/scripts/menu.ts
@@ -1,8 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let isMenuOpen = false;
     const burger = document.querySelector('.header__burger');
     const nav = document.querySelector('.header__nav');
+    const links = document.querySelectorAll<HTMLElement>('.nav__link');
+
 
     burger.addEventListener('click', () => {
-        nav.classList.toggle('header__nav--active');
+        isMenuOpen = !isMenuOpen;
+        if (isMenuOpen) {
+            document.body.style.overflow = 'hidden';
+            nav.classList.toggle('header__nav--active');
+        } else {
+            document.body.style.overflow = 'auto';
+            nav.classList.toggle('header__nav--active');
+        }
+
     });
+
+    if (links && nav) {
+        links.forEach((link) => {
+            link.addEventListener('click', () => {
+                isMenuOpen = !isMenuOpen;
+                document.body.style.overflow = 'auto';
+                nav.classList.toggle('header__nav--active');
+            });
+        });
+    }
+
 });
+
+
+
+
